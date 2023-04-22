41e7c674732d3665425fa2321b84b1e8709d1c00
gdguesser <gdguesser@gmail.com>
2023-04-22 16:51:12 +0100

    implement func for new segment

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -1,5 +1,11 @@
 package log
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 type segment struct {
 	store                  *store
 	index                  *index
@@ -7,4 +13,40 @@ type segment struct {
 	config                 Config
 }
 
+func newSegment(dir string, baserOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baserOffset,
+		config:     c,
+	}
 
+	var err error
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baserOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baserOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baserOffset
+	} else {
+		s.nextOffset = baserOffset + uint64(off) + 1
+	}
+
+	return s, nil
+}
